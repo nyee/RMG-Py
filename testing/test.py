13e2fa4c06356f3bca000eafc93b7e42afcfe6bb
Connie Gao <connieg@mit.edu>
2016-02-22 21:58:40 -0500

    Some modifications to make the plots be in the right timescale and have minor diffs

--- testing/test.py
+++ testing/test.py
@@ -1,11 +1,11 @@
 import regressiveTest as rt
 
 #Test Condition class
-x=rt.Condition('IdealGasReactor', 5, {'Ar':0.95, 'X':0.05}, 900.0, 100000.0, 0.09)
-print x.__repr__()
-print x.__str__()
+#x=rt.Condition('IdealGasReactor', 5, {'Ar':0.95, 'X':0.05}, 900.0, 100000.0, 0.09)
+#print x.__repr__()
+#print x.__str__()
 
-rt.Condition(reactorType="IdealGasReactor", reactionTime=5, molFrac={'X': 0.05, 'Ar': 0.95}, T0=900.0000000000, P0=100000.0000000000, V0=0.0900000000)
+#rt.Condition(reactorType="IdealGasReactor", reactionTime=5, molFrac={'X': 0.05, 'Ar': 0.95}, T0=900.0000000000, P0=100000.0000000000, V0=0.0900000000)
 
 #Test generateAllConditions
 # molFracList=[{'X': 0.05, 'Ar': 0.95}, {'X': 0.025, 'Ar': 0.975}]
@@ -19,7 +19,8 @@ rt.Condition(reactorType="IdealGasReactor", reactionTime=5, molFrac={'X': 0.05, 
 molFracList=[{'CC': 0.05, 'Ar': 0.95}]
 Plist=[278643.8]
 Tlist=range(1100,1300,100)
-conditions=rt.generateAllConditions("IdealGasReactor", 0.005, molFracList, Tlist, Plist)
+terminationTime = 5e-5
+conditions=rt.generateAllConditions("IdealGasReactor", terminationTime, molFracList, Tlist, Plist)
 
 #test ObservablesTestCase
 majorSpeciesSmiles=['CC', 'C=C', 'C#C','C', '[CH3]', 'C[CH2]', '[H]', '[H][H]', 'C=[CH]', 'CC[CH2]', 'Ar']
